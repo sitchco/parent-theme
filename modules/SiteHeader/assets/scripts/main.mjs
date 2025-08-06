50dd6464e7c535f97553523ec521a211142e6101
vragosta <vragosta@situationinc.com>
2025-08-06 16:48:00 -0400

    fix javascript

--- modules/SiteHeader/assets/scripts/main.mjs
+++ modules/SiteHeader/assets/scripts/main.mjs
@@ -1,11 +1,8 @@
-const { addAction } = window.wp.hooks.createHooks();
-
-export function registerHasStickyHeaderActions() {
-    addAction('scroll', () => {
+function registerHasStickyHeaderActions() {
+    window.sitchco.hooks.addAction(window.sitchco.constants.SCROLL, () => {
         const isSticky = window.scrollY > 10;
         document.body.classList.toggle('has-sticky-header', isSticky);
     });
 }
 
-const register = (cb) => addAction('initRegister', cb, 100);
-register(registerHasStickyHeaderActions);
+window.sitchco.register(registerHasStickyHeaderActions);
