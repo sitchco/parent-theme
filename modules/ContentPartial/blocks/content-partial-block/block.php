371372aa522e24d1ce62ce6ada2adb673f5ab0c7
Cyrus Collier <ccollier@situationinteractive.com>
2025-11-12 15:29:44 -0600

    Update block wrapper attributes usage

--- modules/ContentPartial/blocks/content-partial-block/block.php
+++ modules/ContentPartial/blocks/content-partial-block/block.php
@@ -9,17 +9,9 @@ use Psr\Container\ContainerInterface;
 use Sitchco\Parent\Modules\ContentPartial\ContentPartialPost;
 use Sitchco\Parent\Modules\ContentPartial\ContentPartialRepository;
 
-$wrapper_attributes = [
-    'class' => 'wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained',
-];
-if (!empty($block['anchor'])) {
-    $wrapper_attributes['id'] = $block['anchor'];
-}
-
 $blockPostId = $context['fields']['block'];
 $Repository = $container->get(ContentPartialRepository::class);
 $post = $Repository->findById($blockPostId);
 if ($post instanceof ContentPartialPost) {
     $context['partial_post'] = $post;
-    $context['wrapper_attributes'] = $wrapper_attributes;
 }
