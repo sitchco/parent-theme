520fb25710560cf111bd93ae7c31bf2c49c31361
vragosta <vragosta@situationinc.com>
2025-12-18 10:27:48 -0500

    simplify heading formats to just apply heading class and leave font size up to the paragraph block (heading.jsx)

--- modules/Theme/assets/scripts/editor-ui/heading.jsx
+++ modules/Theme/assets/scripts/editor-ui/heading.jsx
@@ -5,24 +5,9 @@ import { RichTextToolbarButton } from '@wordpress/block-editor';
 domReady(() => {
     const headingFormats = [
         {
-            name: 'sitchco/heading-style-default',
-            title: 'Heading Default',
-            className: 'is-styled-as-heading',
-        },
-        {
-            name: 'sitchco/heading-style-small',
-            title: 'Heading Small',
-            className: 'is-styled-as-heading-small',
-        },
-        {
-            name: 'sitchco/heading-style-large',
-            title: 'Heading Large',
-            className: 'is-styled-as-heading-large',
-        },
-        {
-            name: 'sitchco/heading-style-xlarge',
-            title: 'Heading X-Large',
-            className: 'is-styled-as-heading-xlarge',
+            name: 'sitchco/heading-style',
+            title: 'Heading',
+            className: 'wp-block-heading',
         },
     ];
 
