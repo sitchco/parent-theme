f10f17ba502b2ae43b6b49854e933686e0e46a32
Jeremy Strom <jstrom@situationinteractive.com>
2026-01-30 09:54:50 -0600

    Update packages

--- modules/KadenceBlocks/assets/scripts/editor-ui.js
+++ modules/KadenceBlocks/assets/scripts/editor-ui.js
@@ -1,7 +1,6 @@
 import './editor-ui/column-gap-defaults.js';
 import './editor-ui/measure-css-variables.js';
 import './editor-ui/overrides.js';
-
 import extendKadenceColumnBackground from './editor-ui/kadence-column-background.jsx';
 import extendKadenceRowSubgrid from './editor-ui/kadence-row-subgrid.jsx';
 
