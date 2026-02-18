ea272983de107b7e9c989c4e4a696159fc36cd95
Jeremy Strom <jstrom@situationinteractive.com>
2026-02-17 18:22:44 -0600

    Prevent unnecessary loop cloning

--- modules/ContentSlider/blocks/content-slider/script.js
+++ modules/ContentSlider/blocks/content-slider/script.js
@@ -5,7 +5,7 @@
  * Initializes Splide carousels on elements with data-sc-slider attribute.
  * Depends on kad-splide (Kadence's Splide library) which provides global Splide.
  *
- * Filter: 'contentSlider.config' - Modify Splide config before initialization
+ * Filter: 'content-slider.config' - Modify Splide config before initialization
  * @param {Object} config - The Splide configuration object
  * @param {HTMLElement} element - The slider element
  * @returns {Object} Modified config
@@ -41,7 +41,17 @@ function initSlider(element) {
         };
 
         // Allow child themes to filter the config
-        config = applyFilters('contentSlider.config', config, element);
+        config = applyFilters('content-slider.config', config, element);
+
+        // Prevent loop cloning when all items fit on screen
+        const slideCount = element.querySelectorAll('.splide__slide').length;
+        const maxPerPage = Math.max(
+            config.perPage,
+            ...Object.values(config.breakpoints || {}).map((bp) => bp.perPage || 0)
+        );
+        if (slideCount <= maxPerPage) {
+            config.type = 'slide';
+        }
 
         const splide = new Splide(element, config);
 
