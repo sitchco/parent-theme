cb0c407b647c05d96dfb5c37f5f4ee32df9a06f5
Jeremy Strom <jstrom@situationinteractive.com>
2026-03-12 12:41:28 -0500

    Make modal slugs editable

--- modules/ContentPartial/ContentPartialModule.php
+++ modules/ContentPartial/ContentPartialModule.php
@@ -25,7 +25,16 @@ class ContentPartialModule extends Module
         if (is_admin()) {
             add_action('current_screen', [$this->contentService, 'ensureTaxonomyTermExists']);
             add_action('current_screen', [$this->contentService, 'registerBlockPatterns']);
+            add_filter('is_post_type_viewable', [$this, 'makeSlugEditable'], 10, 2);
         }
         add_action('wp', [$this->contentService, 'setContext']);
+    }
+
+    public function makeSlugEditable(bool $is_viewable, \WP_Post_Type $post_type): bool
+    {
+        if ($post_type->name === ContentPartialPost::POST_TYPE) {
+            return true;
+        }
+        return $is_viewable;
     }
 }
